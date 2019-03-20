7801bf78f6a7f0500d0a34d4cb76e3c73719b6f8
Lewis Thomson <1601828@uad.ac.uk>
2019-03-20 13:03:36 +0000

    Added the ACTUAL project.
    Welp, this'll be fun.

---
+++ FuzzyCarLogic/FuzzyCarLogic/Main.cpp
@@ -0,0 +1,21 @@
+//help
+
+#include "fl/Headers.h"
+
+
+//For Loading in FIS file.
+//int main(int argc, char **argv)
+//{
+//fl::Engine* engine = fl::FisImporter().fromFile("MatL.fis");
+//
+//	engine->getInputVariable(0)->setValue(2);
+//	engine->getInputVariable(1)->setValue(-1);
+//
+//	engine->process();
+//
+//	fl::OutputVariable* output = engine->getOutputVariable(0);
+//
+//	printf("%f\n", output->getValue());
+//
+//	system("PAUSE");
+//}
