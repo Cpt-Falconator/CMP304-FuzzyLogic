3eb067bceb79a0dfe6822e1451e29e8d1399a6ca
Lewis Thomson <1601828@uad.ac.uk>
2019-03-22 14:29:54 +0000

    ¯\_(ツ)_/¯

--- FuzzyCarLogic/FuzzyCarLogic/Main.cpp
+++ FuzzyCarLogic/FuzzyCarLogic/Main.cpp
@@ -1,9 +1,10 @@
-//help
-
 #include "fl/Headers.h"
-
+#include <SFML/Graphics.hpp>
 
 //For Loading in FIS file.
+
+
+//This shit goes in update function, saving here for future use
 //int main(int argc, char **argv)
 //{
 //fl::Engine* engine = fl::FisImporter().fromFile("MatL.fis");
@@ -19,3 +20,44 @@
 //
 //	system("PAUSE");
 //}
+
+using namespace std;
+
+//Completely reworked project. Only borrowing the Input class from previous submission.
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(600, 300), "Pong Server");
+
+	sf::Clock clock;
+	float deltaTime;
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+
+				//Handles events of the key being pressed
+			case sf::Event::KeyPressed:
+				input.setKeyDown(event.key.code);
+				break;
+
+				//Handles events of the key being released
+			case sf::Event::KeyReleased:
+				input.setKeyUp(event.key.code);
+			default:
+				break;
+			}
+
+		}
+
+		deltaTime = clock.restart().asSeconds();
+
+	}
+}
