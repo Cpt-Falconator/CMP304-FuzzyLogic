4c6bf6034490e7d4f794b9d13eee291bb7e37617
Lewis Thomson <1601828@uad.ac.uk>
2019-04-01 13:56:31 +0100

    Finished development for now
    Added Stage 1,
    Updated Blogs

--- FuzzyCarLogic/FuzzyCarLogic/Main.cpp
+++ FuzzyCarLogic/FuzzyCarLogic/Main.cpp
@@ -1,26 +1,10 @@
 #include "Input.h"
 #include "Demo.h"
+#include "fl/Headers.h"
 #include <SFML/Graphics.hpp>
 
+
 //For Loading in FIS file.
-
-
-//This shit goes in update function, saving here for future use
-//int main(int argc, char **argv)
-//{
-//fl::Engine* engine = fl::FisImporter().fromFile("MatL.fis");
-//
-//	engine->getInputVariable(0)->setValue(2);
-//	engine->getInputVariable(1)->setValue(-1);
-//
-//	engine->process();
-//
-//	fl::OutputVariable* output = engine->getOutputVariable(0);
-//
-//	printf("%f\n", output->getValue());
-//
-//	system("PAUSE");
-//}
 
 //Completely reworked project. Only borrowing the Input class from previous submission.
 
@@ -31,6 +15,12 @@ int main()
 	Demo demo(&window, &input);
 	sf::Clock clock;
 	float deltaTime;
+	int stage;
+
+	std::cout << "Please select which stage to test:" << std::endl;
+	std::cout << "Stage (1) - Manual Input." << std::endl;
+	std::cout << "Stage (2) - Interactive Input." << std::endl;
+	std::cin >> stage;
 
 	while (window.isOpen())
 	{
@@ -56,10 +46,34 @@ int main()
 			}
 
 		}
-
-		deltaTime = clock.restart().asSeconds();
-		demo.handleInput(deltaTime);
-		demo.update(deltaTime);
-		demo.render();
+		switch (stage)
+		{
+		case 1:
+			fl::Engine* fisEngine;
+			fisEngine = fl::FisImporter().fromFile("FuzzyCar.fis");
+			fl::OutputVariable* fisOutput;
+			float distance, cspeed;
+			std::cout << "Input 1 - Please enter the distance from the line." << std::endl;
+			std::cout << "(Range: -10, 10)" << std::endl;
+			std::cin >> distance;
+			fisEngine->getInputVariable(0)->setValue(distance);
+			std::cout << "Input 2- Please enter current speed." << std::endl;
+			std::cin >> cspeed;
+			fisEngine->getInputVariable(1)->setValue(cspeed);
+			fisEngine->process();
+			fisOutput = fisEngine->getOutputVariable(0);
+			std::cout << "Output - Velocity: " << fisOutput->getValue() << std::endl;
+			std::cout << "Negative value is acceleration to the left, Positive value is acceleration to the right." << std::endl;
+			break;
+		case 2:
+			deltaTime = clock.restart().asSeconds();
+			demo.handleInput(deltaTime);
+			demo.update(deltaTime);
+			demo.render();
+			break;
+		default:
+			std::cout << "Invalid stage, please enter a valid input!" << std::endl;
+			break;
+	}
 	}
 }
